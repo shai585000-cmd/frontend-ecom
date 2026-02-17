b7d8048027eef652feb0663ec783473285ebd7d8
OFFO ANGE EMMANUEL <andrepierre585@gmail.com>
2026-02-17 01:08:48 +0000

    Implement Google OAuth client-side with @react-oauth/google

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
+const GOOGLE_CLIENT_ID = '427788449898-u3i5tqe9dnpvice4kjr3rp06vbfou4sv.apps.googleusercontent.com'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <Toaster 
       position="top-right"
       toastOptions={{
@@ -37,5 +41,6 @@ createRoot(document.getElementById('root')).render(
       }}
     />
     <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
